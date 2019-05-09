b3f75dcfb51821aa6e7f01780d3c0303c9935fd7
David Arutiunian <arutunjan666@mail.com>
2019-05-09 15:15:36 +0300

    fix(logo): fix for lazy-line-painter bug
    fix lazy-line-painter https://github.com/camoconnell/lazy-line-painter/issues/74

--- src/components/logo.js
+++ src/components/logo.js
@@ -16,12 +16,22 @@ function Logo({ duration, delay, onInitialTweenComplete = noop }) {
             import("../vendor/lazy-line-painter-1.9.6.min.js").then(
                 ({ default: LazyLinePainter }) => {
                     imported.current = true;
+
+                    // fix for https://github.com/camoconnell/lazy-line-painter/issues/74
+                    const svg = document.querySelector("#penrose");
+                    const clone = svg.cloneNode(true);
+
                     LazyLinePainterImpl.current = LazyLinePainter;
                     const tween = paint(LazyLinePainter);
+
                     tween.on("complete", () => {
                         onInitialTweenComplete();
                         playing.current = false;
-                        tween.erase();
+
+                        const wrapper = document.querySelector("#penrose__wrapper");
+                        wrapper.appendChild(clone);
+
+                        tween.destroy();
                     });
                 }
             );
@@ -49,60 +59,62 @@ function Logo({ duration, delay, onInitialTweenComplete = noop }) {
     });
 
     return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            version="1.0"
-            width="526.36401"
-            height="477.03693"
-            id="penrose"
-            data-llp-composed="true"
-            className="lazy-line-painter"
-        >
-            <g transform="translate(-91.423882,-253.5866)">
-                <path
-                    d="M 312.32408,254.29303 L 497.87767,584.55178 L 291.61277,584.66663 L 252.04899,655.38029 L 617.26698,655.43727 L 394.46457,254.0866 L 312.32408,254.29303 z "
-                    css={css`
-                        fill: transparent;
-                        fill-opacity: 1;
-                        fill-rule: evenodd;
-                        stroke-linecap: butt;
-                        stroke-linejoin: round;
-                    `}
-                    data-llp-id="penrose-0"
-                    data-llp-duration={duration}
-                    data-llp-delay={delay}
-                    fillOpacity="0"
-                />
-                <path
-                    d="M 312.31392,254.26591 L 91.923882,655.37223 L 132.32998,728.10322 L 315.16759,396.77318 L 417.193,584.66155 L 498.0052,584.66155 L 312.31392,254.26591 z "
-                    css={css`
-                        fill: transparent;
-                        fill-opacity: 1;
-                        fill-rule: evenodd;
-                        stroke-linecap: butt;
-                        stroke-linejoin: round;
-                    `}
-                    data-llp-id="penrose-1"
-                    data-llp-duration={duration}
-                    data-llp-delay={delay}
-                    fillOpacity="0"
-                />
-                <path
-                    d="M 315.20979,396.83568 L 355.50104,471.05493 L 251.87272,655.43409 L 617.28788,655.62366 L 578.81741,730.12352 L 132.3464,728.07665 L 315.20979,396.83568 z "
-                    css={css`
-                        fill: transparent;
-                        fill-opacity: 1;
-                        fill-rule: evenodd;
-                        stroke-linecap: butt;
-                        stroke-linejoin: round;
-                    `}
-                    data-llp-id="penrose-2"
-                    data-llp-duration={duration}
-                    data-llp-delay={delay}
-                    fillOpacity="0"
-                />
-            </g>
-        </svg>
+        <div id="penrose__wrapper">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.0"
+                width="526.36401"
+                height="477.03693"
+                id="penrose"
+                data-llp-composed="true"
+                className="lazy-line-painter"
+            >
+                <g transform="translate(-91.423882,-253.5866)">
+                    <path
+                        d="M 312.32408,254.29303 L 497.87767,584.55178 L 291.61277,584.66663 L 252.04899,655.38029 L 617.26698,655.43727 L 394.46457,254.0866 L 312.32408,254.29303 z "
+                        css={css`
+                            fill: transparent;
+                            fill-opacity: 1;
+                            fill-rule: evenodd;
+                            stroke-linecap: butt;
+                            stroke-linejoin: round;
+                        `}
+                        data-llp-id="penrose-0"
+                        data-llp-duration={duration}
+                        data-llp-delay={delay}
+                        fillOpacity="0"
+                    />
+                    <path
+                        d="M 312.31392,254.26591 L 91.923882,655.37223 L 132.32998,728.10322 L 315.16759,396.77318 L 417.193,584.66155 L 498.0052,584.66155 L 312.31392,254.26591 z "
+                        css={css`
+                            fill: transparent;
+                            fill-opacity: 1;
+                            fill-rule: evenodd;
+                            stroke-linecap: butt;
+                            stroke-linejoin: round;
+                        `}
+                        data-llp-id="penrose-1"
+                        data-llp-duration={duration}
+                        data-llp-delay={delay}
+                        fillOpacity="0"
+                    />
+                    <path
+                        d="M 315.20979,396.83568 L 355.50104,471.05493 L 251.87272,655.43409 L 617.28788,655.62366 L 578.81741,730.12352 L 132.3464,728.07665 L 315.20979,396.83568 z "
+                        css={css`
+                            fill: transparent;
+                            fill-opacity: 1;
+                            fill-rule: evenodd;
+                            stroke-linecap: butt;
+                            stroke-linejoin: round;
+                        `}
+                        data-llp-id="penrose-2"
+                        data-llp-duration={duration}
+                        data-llp-delay={delay}
+                        fillOpacity="0"
+                    />
+                </g>
+            </svg>
+        </div>
     );
 }
 
